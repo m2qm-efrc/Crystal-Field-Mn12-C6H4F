d0151a4f2b4a166f7d7724a808be9b5b3dec2f24
Dmitry Skachkov <d.g.skachkov@gmail.com>
2022-04-27 21:09:13 -0400

    Update CrystalField.f90

--- src/CrystalField.f90
+++ src/CrystalField.f90
@@ -6,7 +6,7 @@
        use Mn12_crystal                         
        use E_field           
        use Plot_results
-       call set_crystal_param                       ! crystall parameters
+       call set_crystal_param                       ! crystal parameters
        call calc_xy_mesh                            ! calculate xy mesh
        call calc_efield_xy                          ! calculate E field on xy mesh points
        call plot_E                                  ! write data for plotting
