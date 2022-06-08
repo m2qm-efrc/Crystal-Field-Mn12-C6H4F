99e66d17fb0eb16aac9fe1d5534a2c75f46d6f40
Dmitry Skachkov <d.g.skachkov@gmail.com>
2022-06-08 00:19:29 -0400

    Update CrystalField.f90

--- src/CrystalField.f90
+++ src/CrystalField.f90
@@ -3,7 +3,7 @@
 ! Copyright (C) 2022   Center for Molecular Magnetic Quantum Materials (M2QM),
 !                      Quantum Theory Project (QTP),
 !                      Department of Physics, University of Florida, Gainesville, FL 32611, USA 
-!                      [https://efrc.ufl.edu/](https://efrc.ufl.edu/)
+!                      https://efrc.ufl.edu/
 
       Program CrystalField                          ! calculate crystal field  for Mn12 crystall        
        use Mn12_crystal                         
